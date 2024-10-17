35f03da03d5159f08ed01bcccd9fc0150848a6d7
RenaldoNika <roniii2017@icloud.com>
2024-10-17 22:23:08 +0200

    11

--- src/main/java/FirstProject/demo/Entity/PersonController.java
+++ src/main/java/FirstProject/demo/Entity/PersonController.java
@@ -4,7 +4,6 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
@@ -69,7 +68,6 @@ public class PersonController {
     public List<Person> personList() {
         List<Person> personList = personRepository.findAll();
         System.out.println(personList);
-
         return personList;
     }
 
