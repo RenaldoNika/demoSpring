db7b609f65ae2cbe3fe15a9b3be4236896b2cace
RenaldoNika <roniii2017@icloud.com>
2024-10-19 22:51:07 +0200

    1.0

--- src/main/java/FirstProject/demo/Entity/PersonNotFoundException.java
+++ src/main/java/FirstProject/demo/Entity/PersonNotFoundException.java
@@ -1,9 +1,15 @@
 package FirstProject.demo.Entity;
 
-public class PersonNotFoundException extends RuntimeException{
 
+
+
+public class PersonNotFoundException extends RuntimeException{
 
     public PersonNotFoundException(String message){
         super(message);
+    }
+
+    public PersonNotFoundException() {
+        super("Person not found.");
     }
 }
